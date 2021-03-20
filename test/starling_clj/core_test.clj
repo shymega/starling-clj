548ef875d415af70f665b3f87a0dadbe8f285a4c
Dom Rodriguez <shymega@shymega.org.uk>
2021-03-20 19:52:27 +0000

    Remove tests temporarily
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- test/starling_clj/core_test.clj
+++ test/starling_clj/core_test.clj
@@ -2,7 +2,3 @@
   (:require [clojure.test :refer :all]
             [starling-clj.core :refer :all]))
 
-
-(deftest a-test
-  (testing "FIXME, I don't fail."
-    (is (true? (= 4 (count (str (= 1 1))))))))
