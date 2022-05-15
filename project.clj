7c9db7fb579b46d4fb01387794f9b2bf75aa762b
Dom Rodriguez <shymega@shymega.org.uk>
2022-05-15 22:59:56 +0100

    Relicense to MIT
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- project.clj
+++ project.clj
@@ -2,9 +2,7 @@
   :description "Clojure library for Starling's API"
   :url "https://github.com/shymega/starling-clj"
   :licenses [{:name "The MIT License"
-              :url "https://opensource.org/licenses/MIT"}
-             {:name "Apache License 2.0"
-              :url "https://www.apache.org/licenses/LICENSE-2.0"}]
+              :url "https://opensource.org/licenses/MIT"}]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [timbre "0.5.1-SNAPSHOT"]
                  [clj-http "3.10.2"]]
